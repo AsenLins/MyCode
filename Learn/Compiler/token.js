c732cb462715358e38d40b26d3f0cfa84806987c
CHZA林根森-Kim <mzh1708587@centaline.com.cn>
2019-01-11 18:05:08 +0800

    update vue-blog-ui

---
+++ Learn/Compiler/token.js
@@ -0,0 +1,62 @@
+function token(inputStr){
+    if(inputStr===undefined||inputStr===""){
+        return null;
+    }
+    
+    var current=0;
+    var tokens=[];
+
+    while(current<inputStr.length){
+        
+        var char=inputStr[current];
+
+
+        if(char==="("){
+            tokens.push({
+                type:"paren",
+                value:"("
+
+            });
+
+            current++;
+            continue;
+        }
+
+        if(char===")"){
+            tokens.push({
+                type:"paren",
+                value:")"
+            });
+        }
+
+
+
+        /*这里选择的是跳过空白字符 */
+        if(/\s/.test(char)){
+            current++;
+            continue;
+        }
+
+
+        /*验证是否为数字 */
+        if(/[0-9]/.test(char)){
+            var vaule="";
+
+            vaule=vaule+inputStr[current++];
+
+            tokens.push({
+                              
+            });
+            
+
+        }
+
+
+       
+
+    }
+    
+
+
+
+}
