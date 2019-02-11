ef5f26ddbbc67d7c76a571c9a8531a6dd029fd51
CHZA林根森-Kim <mzh1708587@centaline.com.cn>
2019-02-11 14:17:38 +0800

    update vue ssr demo

---
+++ Learn/Vue/VueSSR/server.js
@@ -0,0 +1,12 @@
+// server.js
+const createApp = require('./app')
+
+server.get('*', (req, res) => {
+  const context = { url: req.url }
+  const app = createApp(context)
+
+  renderer.renderToString(app, (err, html) => {
+    // 处理错误……
+    res.end(html)
+  })
+})
